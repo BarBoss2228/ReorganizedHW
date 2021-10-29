4063c49992e8ae94a2e0621730c23011548e4231
Anastasia <nastia.polpoudenko@gmail.com>
2021-10-29 18:14:17 +0300

    Finished writing tests

--- Homework1Tests/HW1Tests.cs
+++ Homework1Tests/HW1Tests.cs
@@ -22,5 +22,41 @@ namespace Homework1Tests
             Assert.AreEqual(expected, actual);
 
         }
+        /*[TestCase()]
+        public void SwapNumbers(ref int a, ref int b)
+        {
+
+        }*/
+        [TestCase(2,4,8,2)]
+        public void SolveLinearEquation(int a,int b, int c, int expected)
+        {
+            //arrange
+            //act
+            int actual = _hw1.SolveLinearEquation(a,b,c);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(2,2,1,1,1)]
+        public void CalculateFactorK(int x1, int y1, int x2, int y2, int expected)
+        {
+            //arrange
+            //act
+            int actual = _hw1.CalculateFactorK(x1, y1, x2, y2);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(2,2,6,2)]
+        public void CalculateFactorB(int k, int x2, int y2, int expected)
+        {
+            //arrange
+            //act
+            int actual = _hw1.CalculateFactorB(k, x2, y2);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
     }
 }
